268891cd183af55fc47b3a5855c98592f76756b4
binik17 <reg0110010@gmail.com>
2026-09-15 00:13:25 +0500

    added -help and stream from video

--- sayo_streaming.c
+++ sayo_streaming.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 #include <libusb-1.0/libusb.h>
 
 #define VID 0x8089
@@ -17,6 +18,29 @@ unsigned char *global_rgb_buffer;
 pthread_mutex_t frame_mutex = PTHREAD_MUTEX_INITIALIZER;
 int pipe_fd = -1;
 
+FILE *global_ffmpeg_pipe = NULL;
+libusb_device_handle *global_dev_handle = NULL;
+libusb_context *global_ctx = NULL;
+
+void clean_on_ex(int signum) {
+    printf("\nCTRL C clean \n");
+
+    if (global_ffmpeg_pipe != NULL) {
+        pclose(global_ffmpeg_pipe);
+    }
+
+    system("pkill -9 -f 'ffmpeg.*rawvideo'");
+    system("pkill -9 -f 'ffmpeg.*video50'");
+
+    if (global_dev_handle != NULL) {
+        libusb_release_interface(global_dev_handle, 1);
+        libusb_attach_kernel_driver(global_dev_handle, 1);
+        libusb_close(global_dev_handle);
+    }
+
+    _Exit(0);
+}
+
 void* video_receiver_thread(void* arg) {
     unsigned char *local_buffer = malloc(rgb_size);
     while (1) {
@@ -24,7 +48,6 @@ void* video_receiver_thread(void* arg) {
         while (bytes_read < rgb_size) {
             int r = read(pipe_fd, local_buffer + bytes_read, rgb_size - bytes_read);
             if (r <= 0) {
-                //usleep(1000);
                 break;
             }
             bytes_read += r;
@@ -40,31 +63,60 @@ void* video_receiver_thread(void* arg) {
     return NULL;
 }
 
-int main() {
-    libusb_context *ctx = NULL;
-    libusb_device_handle *dev_handle = NULL;
+int main(int argc, char *argv[]) {
+    signal(SIGINT, clean_on_ex);
+
     int actual_len;
+    char *vid = NULL;
+    int opt;
+    while ((opt = getopt(argc, argv, "p:h")) != -1) {
+        if (opt == 'p') {
+            vid = optarg;
+        }
+        else if(opt == 'h'){
+            printf("Usage: %s [OPTIONS]\n\n", argv[0]);
+            printf("Options:\n");
+            printf("  -p <path>  Stream directly from a video file (e.g., mp4, avi)\n");
+            printf("  -h         Show this help message and exit\n\n");
+            printf("If no options are provided, the program streams from /dev/video50 by default.\n");
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
 
-    if (libusb_init(&ctx) < 0) return 1;
-    dev_handle = libusb_open_device_with_vid_pid(ctx, VID, PID);
-    if (!dev_handle) {
-        printf("[Error] SayoDevice not found.\n");
-        libusb_exit(ctx);
+    if (libusb_init(&global_ctx) < 0) return 1;
+    global_dev_handle = libusb_open_device_with_vid_pid(global_ctx, VID, PID);
+    if (!global_dev_handle) {
+        printf("SayoDevice not found.\n");
+        libusb_exit(global_ctx);
         return 1;
     }
 
-    if (libusb_kernel_driver_active(dev_handle, 1) == 1) {
-        libusb_detach_kernel_driver(dev_handle, 1);
+    if (libusb_kernel_driver_active(global_dev_handle, 1) == 1) {
+        libusb_detach_kernel_driver(global_dev_handle, 1);
     }
-    libusb_claim_interface(dev_handle, 1);
+    libusb_claim_interface(global_dev_handle, 1);
 
-    // using /dev/video50
-    FILE *ffmpeg_pipe = popen("ffmpeg -fflags nobuffer -flags low_delay -f v4l2 -i /dev/video50 -vf \"scale=160:80\" -f rawvideo -pix_fmt rgb24 pipe:1 2>/dev/null", "r");
-    if (!ffmpeg_pipe) {
-        printf("[Error] Failed to start FFmpeg pipe!\n");
+    char command_buff[512];
+    if (vid != NULL) {
+        snprintf(command_buff, sizeof(command_buff), 
+                 "ffmpeg -re -i %s -vf \"scale=160:80\" -f rawvideo -pix_fmt rgb24 pipe:1 2>/dev/null", vid);
+    } else {
+        snprintf(command_buff, sizeof(command_buff), 
+                 "ffmpeg -fflags nobuffer -flags low_delay -f v4l2 -i /dev/video50 -vf \"scale=160:80\" -f rawvideo -pix_fmt rgb24 pipe:1 2>/dev/null");
+    }
+
+    global_ffmpeg_pipe = popen(command_buff, "r");
+    if (!global_ffmpeg_pipe) {
+        printf("Failed to start FFmpeg pipe\n");
+        libusb_release_interface(global_dev_handle, 1);
+        libusb_close(global_dev_handle);
+        libusb_exit(global_ctx);
         return 1;
     }
-    pipe_fd = fileno(ffmpeg_pipe);
+    pipe_fd = fileno(global_ffmpeg_pipe);
 
     global_rgb_buffer = malloc(rgb_size);
     memset(global_rgb_buffer, 0, rgb_size);
@@ -77,7 +129,7 @@ int main() {
     unsigned char local_rgb[DEV_WIDTH * DEV_HEIGHT * 3];
     unsigned char pack1024[1024];
 
-    printf("[OK] USB streaming has started.\n");
+    printf("USB streaming has started.\n");
 
     while (1) {
         pthread_mutex_lock(&frame_mutex);
@@ -96,7 +148,6 @@ int main() {
             buf_idx += 2;
         }
 
-
         memset(pack1024, 0, 1024);
         pack1024[12] = 0x01; 
         
@@ -109,7 +160,6 @@ int main() {
         pack1024[5] = (total_len_init >> 8) & 0xFF; 
         pack1024[6] = 0x3E;
 
-
         unsigned int hash_init = 0;
         int target_len_init = total_len_init + 4;
         if (target_len_init % 2 != 0) target_len_init++;
@@ -121,13 +171,8 @@ int main() {
         pack1024[2] = hash_init & 0xFF; 
         pack1024[3] = (hash_init >> 8) & 0xFF; 
         
-        libusb_bulk_transfer(dev_handle, EP_OUT, pack1024, 1024, &actual_len, 1000);
-        //usleep(1000); 
+        libusb_bulk_transfer(global_dev_handle, EP_OUT, pack1024, 1024, &actual_len, 1000);
 
-
-        // streaming 
-        // 0x3E: 22 04 65 04 05 00 3E 00 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00
-        // 0x25: 22 04 24 62 FC 03 25 00 00 00 00 00 E3 18 E3 18 E3 18 E3 18 E3 18 E3 18
         for (int offset = 0; offset < sizeof(rgb565); offset += 1012) {
             int chunk = (sizeof(rgb565) - offset) > 1012 ? 1012 : (sizeof(rgb565) - offset);
             memset(pack1024, 0, 1024);
@@ -139,9 +184,8 @@ int main() {
 
             memcpy(&pack1024[12], &rgb565[offset], chunk);
             
-            
             int len_data = chunk + 4; 
-            int total_len_data = len_data + 4; // 1020 bite
+            int total_len_data = len_data + 4; 
             
             pack1024[0] = 0x22; 
             pack1024[1] = 0x04; 
@@ -160,18 +204,9 @@ int main() {
             pack1024[2] = hash_data & 0xFF; 
             pack1024[3] = (hash_data >> 8) & 0xFF; 
             
-            libusb_bulk_transfer(dev_handle, EP_OUT, pack1024, 1024, &actual_len, 1000);
-            //usleep(450); 
+            libusb_bulk_transfer(global_dev_handle, EP_OUT, pack1024, 1024, &actual_len, 1000);
         }
-
-        //usleep(16000); 
     }
 
-    free(global_rgb_buffer);
-    pclose(ffmpeg_pipe);
-    libusb_release_interface(dev_handle, 1);
-    libusb_attach_kernel_driver(dev_handle, 1);
-    libusb_close(dev_handle);
-    libusb_exit(ctx);
     return 0;
 }
